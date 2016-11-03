a85535c025dd83b9ef13aaea0ab3f9f282b7cadc
unknown <nban20@BLRNBAN2075867.sapient.com>
2016-11-03 13:54:07 +0530

    Removed hard coded json object

--- src/components/about-content/scripts/about-content.js
+++ src/components/about-content/scripts/about-content.js
@@ -1,21 +1,12 @@
 /*jshint esversion: 6 */
 
 $(document).ready(function(){
-    let locationData =   {
-        filterList:[{
-            id:"contractor 1",
-            type:"contractor",
-            location:    ['Gopalan Mall', 12.9855300, 77.7083500, 3]
-        },{
-            id:"Distributor 1",
-            type:"distributor",
-            location:    ['UB City', 12.9718230, 77.5960760, 2]
-        },{
-            id:"Distributor 2",
-            type:"distributor",
-            location:    ['HAL', 12.9509290, 77.6679290, 1]
-        }]
-    }; 
+    $.get("http://www.json-generator.com/api/json/get/bPqKNtzqxu?indent=2", function( data ) {
+        if(data && data.filterList){
+            var locations = filterMapLocation(data.filterList,"distributor");
+            renderMap($(".about-map-container"),locations,{});
+        }
+    });
 
     /**
      * filter map locations w.r.t. filter type
@@ -35,8 +26,5 @@ $(document).ready(function(){
 
         return locationData;
     }
-
-    var locations = filterMapLocation(locationData.filterList,"distributor");
-    renderMap($(".about-map-container"),locations,{});
 });
  
