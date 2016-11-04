1cf214e80ca2c4d95d03ba5222034f962417bb1d
Gireesha Mairaje Gopalkrishna <ggopalkrishna@sapient.com>
2016-11-04 13:09:21 +0530

    Added autoprefix module to the framework

--- tasks/less.js
+++ tasks/less.js
@@ -1,22 +1,39 @@
-module.exports = function (gulp, plugins, config) {
-    return function () {
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    autoprefix = new LessPluginAutoPrefix({
+        browsers: ["last 2 versions"]
+    });
+
+module.exports = function(gulp, plugins, config) {
+    return function() {
         return gulp.src(['src/**/*.less', '!src/semantic/**/*.less'])
             .pipe(plugins.lesshint())
             .pipe(plugins.lesshint.reporter('lesshint-reporter-stylish'))
-            .pipe(plugins.sourcemaps.init({loadMaps: true}))
-            .pipe(plugins.less())
+            .pipe(plugins.sourcemaps.init({
+                loadMaps: true
+            }))
+            .pipe(plugins.less({
+                plugins: [autoprefix]
+            }))
 
             .pipe(plugins.concat('styles.css'))
 
             .pipe(gulp.dest(config.paths.dist + config.paths.styles))
 
-            .pipe(plugins.cssmin({sourceMap: true}))
-            .pipe(plugins.header(config.comment, {pkg: config.pkg}))
-            .pipe(plugins.rename({suffix: '.min'}))
+            .pipe(plugins.cssmin({
+                sourceMap: true
+            }))
+            .pipe(plugins.header(config.comment, {
+                pkg: config.pkg
+            }))
+            .pipe(plugins.rename({
+                suffix: '.min'
+            }))
 
             .pipe(plugins.sourcemaps.write('./'))
 
             .pipe(gulp.dest(config.paths.dist + config.paths.styles))
-            .pipe(plugins.browserSync.reload({stream: true}));
+            .pipe(plugins.browserSync.reload({
+                stream: true
+            }));
     }
 }
