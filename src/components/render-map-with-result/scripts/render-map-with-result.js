5a195f95ea10b94edf1d9e02e5e0d7327112b22e
unknown <nban20@BLRNBAN2075867.sapient.com>
2016-11-04 15:48:56 +0530

    Clean up

---
+++ src/components/render-map-with-result/scripts/render-map-with-result.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+
+$(document).ready(function(){
+    $.get("http://www.json-generator.com/api/json/get/cbeCFXZyWa?indent=2", function( data ) {
+        if(data && data.filterList){
+            var locations = filterMapLocation(data.filterList,"distributor");
+            MAP.renderMap($(".map-search-container"),locations,{});
+            renderSearchResultForMap(data);
+            console.log(data);
+            
+        }
+    });
+
+    /**
+     * filter map locations w.r.t. filter type
+     * @param {object} mapData
+     * @param {string} filterType
+     * @returns {object} filtered location array
+     */
+    function filterMapLocation(mapData,filterType){ 
+        let locationData = [];
+        let filteredMapData = mapData.filter((data)=>{ return data.type == filterType;});
+
+        filteredMapData.forEach(function(data){
+            if(data.location){
+                locationData.push(data.location);
+            }
+        });
+
+        return locationData;
+    }
+});
+ 
