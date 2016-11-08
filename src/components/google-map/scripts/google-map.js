d8bf29009c3694c70aa60bd5810662cfd7e3a32e
unknown <nban20@BLRNBAN2075867.sapient.com>
2016-11-08 12:04:53 +0530

    Search map n results initialization

--- src/components/google-map/scripts/google-map.js
+++ src/components/google-map/scripts/google-map.js
@@ -9,15 +9,21 @@
  * @licensor:   SAPIENTNITRO
  *
  */
-
-let MAP = (function(){
+$(document).ready(function(){
+    function loadGoogleMap(){
+        var script_tag = document.createElement('script');
+        script_tag.setAttribute("type","text/javascript");
+        script_tag.setAttribute("src","http://maps.google.com/maps/api/js?key=AIzaSyBLRBD1payUxAa1wpIiT6lMjyP8W2pHrs0");
+        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+    }
+    loadGoogleMap();
 
     /**
      * renders the google map
      * @param {object} locations - list of locations
      * @param {object} config - map configurations
      */
-    let  _renderMap = function(container,locations =[],config = {}){
+    function renderMap(container,locations =[],config = {}){
         if(container && container.length > 0){
             let mapType = config.mapType || "ROADMAP";
             let mapConfig = {
@@ -25,8 +31,8 @@ let MAP = (function(){
                 center: new google.maps.LatLng(config.centerLat || 12.97, config.centerLng || 77.59),
                 mapTypeId : google.maps.MapTypeId[mapType]
             }; 
-
-            let map = new google.maps.Map(container[0], mapConfig),
+            
+            var map = new google.maps.Map(container[0], mapConfig),
                 infowindow = new google.maps.InfoWindow(),
                 marker, i;
 
@@ -53,11 +59,81 @@ let MAP = (function(){
                 })(marker, i));
             }
         }
-    };
-    return{
-        renderMap:_renderMap
-    };
-})();
+    }
+
+    function renderSearchResultForMap(data, resultContainer){
+        let template = resultContainer.find("#search-map-result").html();
+        let compiledCode = Handlebars.compile(template);
+        let result = compiledCode(data);   
+        resultContainer.html(result);
+        $(".ui.rating").rating();
+    }
+
+    /* filter map locations w.r.t. filter type
+     * @param {object} mapData
+     * @param {string} filterType
+     * @returns {object} filtered location array
+     */
+    function filterMapLocation(mapData,filterType){ 
+        let locationData = [];
+        let filteredMapData = mapData.filter((data)=>{ return data.type == filterType;});
+        if(filterType){
+                filteredMapData.forEach(function(data){
+                if(data.location){
+                    locationData.push(data.location);
+                }
+            });
+        }else{
+            mapData.forEach(function(data){
+                if(data.location){
+                    locationData.push(data.location);
+                }
+            });
+        }
+        return locationData;
+    }
+    
+    /**
+    * Retrieves the data from api
+    * @param{string} url - api url
+    * @param{function} callback - callback to be executed after data retrival 
+    * @param{object} locationData - array of locations for google api
+    */
+    function getData(url,callback){
+        $.get(url, function( data ) {
+            if(callback && typeof(callback) === "function"){
+                callback(data);
+            }
+        });
+    }
+
+    var mapContainer = $(".gaf-map-container");
+
+    if(mapContainer.length > 0){
+        $.each(mapContainer,function(index,container){
+            var config = {};
+            config.zoom =  $(container).data("map-zoom") || undefined;
+            config.centerLat = $(container).data("center-lat") || undefined;
+            config.centerLng=$(container).data("center-lng") || undefined;
+            config.mapType = $(container).data("map-type") || undefined;
+            let mapDataURL = $(container).data("href") || undefined;
+            if(mapDataURL){
+                //get data from service
+                 getData(mapDataURL,function(data){
+                    let locations = filterMapLocation(data.filterList);
+                    let mapElement = $(container).find(".map");
+                    //render google map in container
+                    renderMap(mapElement,locations, config);
+                    var searchNearByResults =  $(container).find(".search-result");
+                    if(searchNearByResults && searchNearByResults.length>0){    
+                        renderSearchResultForMap(data,searchNearByResults);
+                    }
+                });
+            }           
+        });
+    } 
+});
+
 
 
 
--- src/components/search-map-result/scripts/search-map-result.js
+++
@@ -1,8 +0,0 @@
-
-function renderSearchResultForMap(data){
-    var template = $("#search-map-result").html();
-    var compiledCode = Handlebars.compile(template);
-    var result = compiledCode(data);
-    $("#search-result-container").html(result);
-    $(".ui.rating").rating();
-}
