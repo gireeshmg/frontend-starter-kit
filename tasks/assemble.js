34972d887f8d4b37fb5a0f1e5eb15cef7731eeef
Gireesha Mairaje Gopalkrishna <ggopalkrishna@sapient.com>
2016-11-09 10:53:52 +0530

    Added handlebar-helper to the framework

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var assemble = require('assemble')();
-//var helpers = require('handlebars-helpers')();
+var helpers = require('handlebars-helpers')();
 
 module.exports = function(gulp, plugins, config) {
     return function() {
@@ -12,7 +12,7 @@ module.exports = function(gulp, plugins, config) {
         ]);
         assemble.pages(path.resolve(config.paths.src, 'templates/pages/*.hbs'));
         assemble.data(path.resolve(config.paths.src, '**/*.json'));
-        //assemble.helpers(helpers);
+        assemble.helpers(helpers);
 
         return assemble.toStream('pages')
             .pipe(assemble.renderFile())
