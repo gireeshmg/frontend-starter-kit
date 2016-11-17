64b64c00309b417343cdcf76f76bbaa30950d1dd
unknown <nban20@BLRNBAN2075867.sapient.com>
2016-11-17 18:28:53 +0530

    In page tab part -2

--- src/components/in-page-tab/scripts/in-page-tab.js
+++ src/components/in-page-tab/scripts/in-page-tab.js
@@ -10,15 +10,33 @@
  *
  */
 $(document).ready(function(){
-    $(".short-names-category column").click(function(){
-        let parentElement = $(this).parent();
-    });
+    var categoryItems = $('.slider-category-names > div');
+    var originalItemOrder = [];
+    if(categoryItems && categoryItems.length >0){
+        var indexOfActiveItem =-1;
+        $.each(categoryItems,function(index,item){
+            if($(item).hasClass("active")){
+                indexOfActiveItem = index;
+            }
+            originalItemOrder.push(item.outerHTML);
+        });
+        if(indexOfActiveItem !== -1){
+            var moveElemsBy = indexOfActiveItem - 1; 
+            console.log("jryufy"+ moveElemsBy)
+            if(moveElemsBy<0 ){
+                moveElemsBy = categoryItems.length;
+                console.log(moveElemsBy);
+            }
+            originalItemOrder = originalItemOrder.concat(originalItemOrder.splice(0,moveElemsBy));         
+        }
+      $('.slider-category-names').html(originalItemOrder.join(" ")); 
+    }
 
     $('.slider-category-names').slick({
         dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 3,
-        slidesToScroll: 1,
-    });
+        slidesToScroll: 1
+    });   
 });
